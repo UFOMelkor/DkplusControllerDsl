be48f15ef442a4249ab5edfd99e8feed76c85fbd
Oskar Bley <oskar@programming-php.net>
2012-09-25 13:05:04 +0200

    core implemented (5): ContainerAwarePhraseInterface

---
+++ src/DkplusControllerDsl/Dsl/Executor/ContainerInjectionExecutor.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * @category   Dkplus
+ * @package    ControllerDsl
+ * @subpackage Dsl
+ * @author     Oskar Bley <oskar@programming-php.net>
+ */
+
+namespace DkplusControllerDsl\Dsl\Executor;
+use DkplusControllerDsl\Dsl\Phrase\PhraseInterface as Phrase;
+use DkplusControllerDsl\Dsl\Phrase\ContainerAwarePhraseInterface as ContainerAwarePhrase;
+use DkplusControllerDsl\Dsl\ContainerInterface as Container;
+/**
+ * @category   Dkplus
+ * @package    ControllerDsl
+ * @subpackage Dsl
+ * @author     Oskar Bley <oskar@programming-php.net>
+ */
+class ContainerInjectionExecutor implements ExecutorInterface
+{
+    /** @var Phrase[] */
+    private $phrases = array();
+
+    /** @var ExecutorInterface */
+    private $decorated;
+
+    public function __construct(ExecutorInterface $decorated)
+    {
+        $this->decorated = $decorated;
+    }
+
+    public function addPhrase(Phrase $phrase)
+    {
+        $this->phrases[] = $phrase;
+    }
+
+    public function execute(Container $container)
+    {
+        foreach ($this->phrases as $phrase) {
+            if ($phrase instanceof ContainerAwarePhrase) {
+                $phrase->setContainer($container);
+            }
+            $this->decorated->addPhrase($phrase);
+        }
+        return $this->decorated->execute($container);
+    }
+}
+
---
+++ src/DkplusControllerDsl/Dsl/Phrase/ContainerAwarePhraseInterface.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @category   Dkplus
+ * @package    ControllerDsl
+ * @subpackage Dsl\Phrase
+ * @author     Oskar Bley <oskar@programming-php.net>
+ */
+
+namespace DkplusControllerDsl\Dsl\Phrase;
+
+use DkplusControllerDsl\Dsl\ContainerInterface as Container;
+
+/**
+ * @category   Dkplus
+ * @package    ControllerDsl
+ * @subpackage Dsl\Phrase
+ * @author     Oskar Bley <oskar@programming-php.net>
+ */
+interface ContainerAwarePhraseInterface extends PhraseInterface
+{
+    public function setContainer(Container $container);
+}
+
