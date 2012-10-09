b43913b0d67ceaf8cebff62e9dde81e57eea92dc
Oskar Bley <oskar@programming-php.net>
2012-10-10 01:25:16 +0200

    configuration fix

--- config/service.config.php
+++ config/service.config.php
@@ -11,12 +11,14 @@ use Zend\ServiceManager\Config;
 use Zend\ServiceManager\ServiceLocatorInterface as ServiceLocator;
 
 return array(
-    'DkplusControllerDsl\Dsl\Phrase\PhraseManager' => function (ServiceLocator $locator) {
-        $config = $locator->get('Config');
+    'factories' => array(
+        'DkplusControllerDsl\Dsl\Phrase\PhraseManager' => function (ServiceLocator $locator) {
+            $config = $locator->get('Config');
 
-        $result = new PhraseManager(new Config($config['phrases']));
-        $result->setServiceLocator($locator);
-        $result->addPeeringServiceManager($locator);
-        return $result;
-    }
+            $result = new PhraseManager(new Config($config['phrases']));
+            $result->setServiceLocator($locator);
+            $result->addPeeringServiceManager($locator);
+            return $result;
+        }
+    )
 );
