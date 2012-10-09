6afc8c0846da64b137b0ac5938cf1ad334ef39d0
Oskar Bley <oskar@programming-php.net>
2012-10-10 01:04:06 +0200

    configuration fix

--- config/controller_plugin.config.php
+++ config/controller_plugin.config.php
@@ -10,8 +10,10 @@ use DkplusControllerDsl\Controller\Plugin\Dsl;
 use Zend\Mvc\Controller\PluginManager;
 
 return array(
-    'dsl' => function (PluginManager $manager) {
-        $manager = $manager->getServiceLocator()->get('DkplusControllerDsl\Dsl\Phrase\PhraseManager');
-        return new Dsl($manager);
-    }
+    'factories' => array(
+        'dsl' => function (PluginManager $manager) {
+            $manager = $manager->getServiceLocator()->get('DkplusControllerDsl\Dsl\Phrase\PhraseManager');
+            return new Dsl($manager);
+        }
+    )
 );
